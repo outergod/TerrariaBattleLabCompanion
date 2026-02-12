1ec3ee571342a1a2940174a5c0d38ba268a0b78a
Alexander Dorn <ad@not.one>
2026-02-12 13:26:15 +0100

    convert to unix and add Nix support

--- CombatTracker.cs
+++ CombatTracker.cs
@@ -7,9 +7,8 @@ using Terraria.ModLoader;
 
 namespace CombatTracker
 {
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
-	public class CombatTracker : Mod
-	{
+    public class CombatTracker : Mod
+    {
 
-	}
+    }
 }
