aa6b6939d418e0eee3209961cfc2c9627a0ea061
Alexander Dorn <ad@not.one>
2026-05-12 17:28:30 +0200

    chore(nix): switch to flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Project dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, fenix, ... }:
+    let
+      system = "x86_64-linux"; # change as needed
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          omnisharp-roslyn
+          nodejs_24
+          bun
+        ];
+
+        shellHook = ''
+          export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
+          unset SOURCE_DATE_EPOCH
+        '';
+      };
+    };
+}
